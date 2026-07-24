0775ec9e4bb6bcac288faa4b69259c1f9fe492cf
Adenilson Silva <adenilson.silva.dev3561@gmail.com>
2026-07-24 15:10:56 -0300

    feature/CFC-102- swagger implementation category

--- ecafe-api/src/main/java/eCafe/API/category/controller/CategoryController.java
+++ ecafe-api/src/main/java/eCafe/API/category/controller/CategoryController.java
@@ -4,6 +4,10 @@ import eCafe.API.category.dto.CategoryRequest;
 import eCafe.API.category.dto.CategoryResponse;
 import eCafe.API.category.service.CategoryService;
 import eCafe.API.common.constants.ApiRoutes;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -15,47 +19,79 @@ import java.util.List;
 @RequestMapping(ApiRoutes.CATEGORY)
 @RestController
 @AllArgsConstructor
+@Tag(name = "Gerenciamento de categorias")
 public class CategoryController {
+
     private final CategoryService categoryService;
 
+    @Operation(description = "Cadastrar categoria.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "201", description = "Categoria cadastrada com sucesso!"),
+            @ApiResponse(responseCode = "400", description = "Dados inválidos."),
+            @ApiResponse(responseCode = "500", description = "Erro interno do servidor.")
+    })
     @PostMapping
-    public ResponseEntity<CategoryResponse> createCategory(@RequestBody @Valid  CategoryRequest dto){
+    public ResponseEntity<CategoryResponse> createCategory(@RequestBody @Valid CategoryRequest dto) {
 
         CategoryResponse category = categoryService.create(dto);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(category);
     }
 
+    @Operation(description = "Atualizar categoria.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Categoria atualizada com sucesso!"),
+            @ApiResponse(responseCode = "400", description = "Dados inválidos."),
+            @ApiResponse(responseCode = "404", description = "Categoria não encontrada."),
+            @ApiResponse(responseCode = "500", description = "Erro interno do servidor.")
+    })
     @PutMapping("/{id}")
-    public ResponseEntity<CategoryResponse> updateCategory(@PathVariable Long id, @RequestBody CategoryRequest dto){
+    public ResponseEntity<CategoryResponse> updateCategory(@PathVariable Long id,
+                                                           @RequestBody @Valid CategoryRequest dto) {
 
         CategoryResponse updated = categoryService.updateCategory(id, dto);
 
         return ResponseEntity.ok(updated);
     }
 
+    @Operation(description = "Listar todas as categorias.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Categorias retornadas com sucesso."),
+            @ApiResponse(responseCode = "500", description = "Erro interno do servidor.")
+    })
     @GetMapping
-    public ResponseEntity<List<CategoryResponse>> findAll(){
+    public ResponseEntity<List<CategoryResponse>> findAll() {
 
         List<CategoryResponse> category = categoryService.findAll();
+
         return ResponseEntity.ok(category);
     }
 
+    @Operation(description = "Buscar categoria pelo ID.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Categoria encontrada com sucesso."),
+            @ApiResponse(responseCode = "404", description = "Categoria não encontrada."),
+            @ApiResponse(responseCode = "500", description = "Erro interno do servidor.")
+    })
     @GetMapping("/{id}")
-    public ResponseEntity<CategoryResponse> findById( @Valid @PathVariable Long id){
+    public ResponseEntity<CategoryResponse> findById(@PathVariable Long id) {
 
         CategoryResponse category = categoryService.categoryId(id);
 
-        return ResponseEntity.status(HttpStatus.OK).body(category);
+        return ResponseEntity.ok(category);
     }
 
+    @Operation(description = "Excluir categoria pelo ID.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "204", description = "Categoria excluída com sucesso."),
+            @ApiResponse(responseCode = "404", description = "Categoria não encontrada."),
+            @ApiResponse(responseCode = "500", description = "Erro interno do servidor.")
+    })
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> delete(@PathVariable Long id){
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
 
         categoryService.deleteById(id);
 
         return ResponseEntity.noContent().build();
-
     }
-
 }
--- ecafe-api/src/main/java/eCafe/API/category/dto/CategoryRequest.java
+++ ecafe-api/src/main/java/eCafe/API/category/dto/CategoryRequest.java
@@ -1,11 +1,17 @@
 package eCafe.API.category.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 
-public record CategoryRequest (
+@Schema(description = "Dados para cadastro de category")
+public record CategoryRequest(
+
+        @Schema(description = "Cafe", example = "Cafés Tradicionais")
         @NotBlank(message = "Nome é obrigatório!")
         String name,
 
+        @Schema(description = "Descrição da categoria.", example = "Cafés produzidos com grãos selecionados, de alta qualidade e sabor diferenciado.")
         @NotBlank(message = "A descrição é obrigatória!")
         String description
-){}
+) {
+}
